752c1dc7aef7ee3f28e54a4bc8888abe5bba59b7
Niv Meiri <nivmeir@post.bgu.ac.il>
2020-09-13 12:30:03 +0300

    init

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var pages = [
 dict["DFS"]=[4,"https://www.youtube.com/watch?v=iaBEKo5sM7w&ab_channel=GoGATEIIT"]
 dict["BFS"]=[4,"BFS"]
 dict["BEST"]=[4,"BEST"]
-dict["HOFFMAN"]=[4,"HOFFMAN"]
+dict["HOFFMAN"]=[9,"HOFFMAN"]
 function myfunc(){
     let info=prompt(" what meterlias are you searching?")
     let h1=document.createElement("h1")
@@ -14,8 +14,6 @@ function myfunc(){
     let h3=document.createElement("h3")
     try {
         if( dict[info]==null) throw "No Value";
-
-       
       }
       catch(err) {
         console.log(err)
@@ -40,48 +38,6 @@ function like(){
     h2.setAttribute("id","myfunc2")
     h2.append(+dict[info][0] +" LIKES ")
     document.getElementById("flex-box-res2").appendChild(h2)
-
-    
-
 }
 function Addtodict(){
     }
-      
-  
-function validatePIN (pin) {
-    function isCharDigit(NUM){
-        return !!NUM.trim() && NUM > -1;
-      }
-    var n=pin.length
-    if(n!=4 & n!=6 ){return false}
-    for(i=0;i<n;i++){
-        if(! isCharDigit(pin[i])){return false}
-    }
-return true  
-}
-
-/*
-let markMass=89,johnMass=89,markH=1.8,johnH=1.7,bmiM,bmiJ,to_return
-bmiM=BMI(markMass,markH)
-bmiJ=BMI(johnMass,johnMass)
-to_return=bmiM<bmiJ
-function BMI(mass,H){
-  return (mass/(H^2))
-}
-let x=false ==""
-let y=""==false
-*/
-
-/*
-challenge 2!
-
-*/
-let average1=(130+120+120)/3
-let av2=(116+94+123)/3
-let bool=average1>av2
-if(bool){
-  console.log("John has ween with score of : "+average1)
-}
-else{
-console.log("Niv is the best player : "+av2)
-}
