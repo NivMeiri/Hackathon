8c89a0783a49d10e82a35bd36f289bcbe12877ae
Guygool <64363416+Guygool@users.noreply.github.com>
2020-09-13 20:56:51 +0300

    init

--- script.js
+++ script.js
@@ -4,17 +4,38 @@ var pages = [
     'index.html',
     'index2.html',
   ];
-dict["DFS"]=[4,"https://www.youtube.com/watch?v=iaBEKo5sM7w&ab_channel=GoGATEIIT"]
+
+
+  let basicLink =  function(name , url) {
+    this.name = name;
+    this.url = url;
+    this.numOfLikes = 0;
+    this.comments = [];
+  }
+
+  let basicSubject = function(name){
+    this.name= name;
+    this.linkArray = [];
+  }
+
+
+
+dict["DFS"] = new basicSubject("DFS");
+dict["DFS"].linkArray.push(new basicLink("YouTube","https://www.youtube.com/watch?v=iaBEKo5sM7w&ab_channel=GoGATEIIT"));
+dict["DFS"].linkArray.push(new basicLink("Moodle","someLink"));
+//console.log(dict["DFS"])
+//dict["DFS"]=[4,"https://www.youtube.com/watch?v=iaBEKo5sM7w&ab_channel=GoGATEIIT", "link 2", "link 3"]
 dict["BFS"]=[4,"BFS"]
 dict["BEST"]=[4,"BEST"]
 dict["HOFFMAN"]=[9,"HOFFMAN"]
 function showSearch(text){
-  var name =document.getElementById("name")
+  var name =document.getElementById("Search")
   console.log(name.value);
+  searchFunc(name.value)
 }
 
-function myfunc(){
-    info=prompt(" what meterlias are you searching?")
+function searchFunc(info){
+    //info=search
     let h1=document.createElement("h1")
     let h2=document.createElement("h2")
     try {
@@ -24,8 +45,14 @@ function myfunc(){
         console.log(err)
         document.getElementById("flex-box-res").innerHTML = err.message;
       }
-    let text=document.createTextNode(" you are searching for "+info+", That what we found: THE ADDRESS: "+dict[info][1]   )
-    h1.setAttribute("id","myfunc")
+    let text=document.createTextNode(" you are searching for "+info+", That's what we found: "/*+dict[info][1]*/ )
+    console.log(typeof(text));
+    for ( let i =0 ; i< dict[info].linkArray.length ; i++){
+        console.log(dict[info].linkArray[i].name);
+          // text+=(dict[info].linkArray[i].name + "\n");
+          // text+=(dict[info].linkArray[i].url + "n");
+    }
+    h1.setAttribute("id","searchFunc")
     h2.setAttribute("id","myfunc2")
     h2.append(+dict[info][0] +" LIKES ")
     h1.appendChild(text)
